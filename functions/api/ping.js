0e63c95452d7a1dc302f59aa65c91cdfc404e11d
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2026-03-05 12:34:02 +0000

    Añadimos ping.js para comprobar que functions de cloudflare funcione y cambiamos href en header

--- functions/api/ping.js
+++ functions/api/ping.js
@@ -1,3 +1,7 @@
+// Esta función es solo para verivicar que el entorno de Cloudflare Functions está funcionando correctamente.
+// Para probarla accedan a https://koradigitalsolutions.com/api/ping
+// Si les aparece {"ok":true,"pong":true} entonces todo está bien.
+
 export function onRequestGet() {
     return new Response(JSON.stringify({ ok: true, pong: true }), {
         headers: { "content-type": "application/json" },
