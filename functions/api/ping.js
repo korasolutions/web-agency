b26aa94009237adbaa1be89471224fab3d0e5bb6
fmoorenoo <fermontemore@gmail.com>
2026-03-08 18:31:05 +0000

    Versiones en los css/js para forzar recarga en los navegadores y ver los cambios

--- functions/api/ping.js
+++
@@ -1,9 +0,0 @@
-// Esta función es solo para verivicar que el entorno de Cloudflare Functions está funcionando correctamente.
-// Para probarla accedan a https://koradigitalsolutions.com/api/ping
-// Si les aparece {"ok":true,"pong":true} entonces todo está bien.
-
-export function onRequestGet() {
-    return new Response(JSON.stringify({ ok: true, pong: true }), {
-        headers: { "content-type": "application/json" },
-    });
-}
